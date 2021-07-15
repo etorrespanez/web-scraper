3fe85551a9b48e5b2b568e12584d498921a94f35
Neila Torres Panez <84181564+etorrespanez@users.noreply.github.com>
2021-07-15 16:29:35 +0200

    feature(*): Perform filter operations
    Sort data structure according to specific criteria.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer';
 
 (async () => {
   
-  const browser = await puppeteer.launch({headless: true}); // Open browser  
+  const browser = await puppeteer.launch({headless: false}); // Open browser  
   const url = 'https://news.ycombinator.com/';              // Set url
   const page = await browser.newPage();                    // Create new page to work with
 
@@ -12,7 +12,7 @@ import puppeteer from 'puppeteer';
 
   const entries = await page.evaluate(() => {
     const pattern = 'points';
-    
+
     //Simplifies coding by eliminating duplicate code when selecting selectors
 
     /* const items = Array.from(document.querySelectorAll('.itemlist tbody'))
@@ -25,17 +25,22 @@ import puppeteer from 'puppeteer';
 
 
     const mainInfo = Array.from(document.querySelectorAll('tr.athing td.title a.storylink')).map((comment) => comment.innerText.trim());
-    const additionalInfo = Array.from(document.querySelectorAll('td.subtext')).map((comment, index) => {
+    const items = Array.from(document.querySelectorAll('td.subtext')).map((comment, index) => {
 
       return { 
         rank: ++index,
         title: mainInfo[index],
-        score: comment.innerText.indexOf(pattern) >= 0 ? comment.innerText.substr(0, comment.innerText.indexOf(pattern) + pattern.length) : 'no score',
-        comments: comment.innerText.trim().split('|').length === 3 ?  comment.innerText.trim().split('|')[2] : 'no comments'
+        score: comment.innerText.indexOf(pattern) >= 0 ? parseFloat(comment.innerText.substr(0, comment.innerText.indexOf(pattern))) : 0,
+        comments: comment.innerText.trim().split('|').length === 3 ?  parseFloat(comment.innerText.trim().split('|')[2]) : 0
       }
     });
 
-    return additionalInfo;
+    let filterdItems = items.filter(item => (item.title || '').split(' ').length > 5);
+    const sortedItems = items.sort(function(a , b) {
+      return b.score - a.score;
+   });
+
+    return filterdItems;
   });
 
   console.log(entries);
