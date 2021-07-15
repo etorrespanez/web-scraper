0de9bac8935be7a81b72a83068ab9289831284c3
Neila Torres Panez <84181564+etorrespanez@users.noreply.github.com>
2021-07-15 12:30:20 +0200

    feature(.js): Generates necessary data structures.
    Generates the data structure with the fields we want to collect.

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ import puppeteer from 'puppeteer';
 
 (async () => {
   
-  const browser = await puppeteer.launch({headless:false}); // Open browser  
-  const page = await browser.newPage();     // Create new page to work with
+  const browser = await puppeteer.launch({headless: true}); // Open browser  
+  const url = 'https://news.ycombinator.com/';              // Set url
+  const page = await browser.newPage();                    // Create new page to work with
 
-  await page.goto('https://news.ycombinator.com/');
+  await page.goto(url);
   await page.screenshot({ path: 'screenshoot1.jpg'});
 
 
   const entries = await page.evaluate(() => {
-    let items = [];
-    const elements = Array.from(document.querySelectorAll('tr.athing td.title a.storylink')).map((comment) => {
-      return  { title: comment.innerText.trim() };
+    let item = {};
+    const pattern = 'points';
+  
+    const mainInfo = Array.from(document.querySelectorAll('tr.athing td.title a.storylink')).map((comment) => comment.innerText.trim());
+    const additionalInfo = Array.from(document.querySelectorAll('td.subtext')).map((comment, index) => {
+
+      return { 
+        rank: ++index,
+        title: mainInfo[index],
+        score: comment.innerText.indexOf(pattern) >= 0 ? comment.innerText.substr(0, comment.innerText.indexOf(pattern) + pattern.length) : 'no score',
+        comments: comment.innerText.trim().split('|').length === 3 ?  comment.innerText.trim().split('|')[2] : 'no comments'
+      }
     });
-    
-
-
-    return items;
+    //return items;
+    return additionalInfo;
   });
 
+  console.log(entries);
   await browser.close();
 })();
